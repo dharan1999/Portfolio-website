970937039f5455f0261b99599c9cbe52d892d3d4
dharan1999 <dharanthakar96@gmail.com>
2023-03-27 10:37:28 -0400

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Software Developer", "Web Developer", "Forex Trader" ];
+  const toRotate = [ "Software Developer", "Frontend Developer", "Forex Trader" ];
   const period = 2000;
 
   useEffect(() => {
